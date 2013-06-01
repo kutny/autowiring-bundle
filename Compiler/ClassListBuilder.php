f49b82449333b2cb252005c2d7ca8be86aeee7fe
Jiri Koutny <jirkakoutny@gmail.com>
2013-06-01 09:36:02 +0200

    Symfony services are not hard-wired

--- Compiler/ClassListBuilder.php
+++ Compiler/ClassListBuilder.php
@@ -8,65 +8,42 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 class ClassListBuilder
 {
 
-    /**
-     * @SuppressWarnings(PMD.NPathComplexity)
-     * @SuppressWarnings(PMD.CyclomaticComplexity)
-     * @SuppressWarnings(PMD.ExcessiveMethodLength)
-     */
     public function buildClassList(ContainerBuilder $containerBuilder)
     {
         $classes = array();
         $parameterBag = $containerBuilder->getParameterBag();
 
-        foreach ($containerBuilder->getDefinitions() as $id => $definition) {
-            if (!$definition->isPublic()) {
-                continue;
-            }
-
-            // to prevent processing of some Doctrine cached classes
-            if ($definition->getFile() !== null) {
-                continue;
-            }
-
+        foreach ($containerBuilder->getDefinitions() as $serviceId => $definition) {
             $class = $parameterBag->resolveValue($definition->getClass());
 
             if ($class === null) {
                 continue;
             }
 
-            $reflection = new ReflectionClass($class);
-
-            if ($reflection) {
-                $classes[$reflection->getName()][] = $id;
-
-                foreach ($reflection->getInterfaceNames() as $interface) {
-                    $classes[$interface][] = $id;
-                }
-
-                $parent = $reflection;
-
-                while (($parent = $parent->getParentClass())) {
-                    $classes[$parent->getName()][] = $id;
-                }
-            }
+            $this->processClass($serviceId, $class, $classes);
         }
-
-        $classes += $this->getSymfonyFrameworkServices();
 
         return $classes;
     }
 
-    private function getSymfonyFrameworkServices()
+    private function processClass($serviceId, $class, array &$classes)
     {
-        $classes = array();
+        $reflection = new ReflectionClass($class);
 
-        $classes['Symfony\Bundle\TwigBundle\TwigEngine'][] = 'templating';
-        $classes['Symfony\Bundle\FrameworkBundle\Routing\Router'][] = 'router';
-        $classes['Symfony\Bundle\FrameworkBundle\HttpKernel'][] = 'http_kernel';
-        $classes['Symfony\Component\Form\FormFactory'][] = 'form.factory';
-        $classes['Doctrine\Bundle\DoctrineBundle\Registry'][] = 'doctrine';
-        $classes['Symfony\Component\Security\Core\SecurityContext'][] = 'security.context';
+        if (!$reflection) {
+            return;
+        }
 
-        return $classes;
+        $classes[$reflection->getName()][] = $serviceId;
+
+        foreach ($reflection->getInterfaceNames() as $interface) {
+            $classes[$interface][] = $serviceId;
+        }
+
+        $parent = $reflection;
+
+        while (($parent = $parent->getParentClass())) {
+            $classes[$parent->getName()][] = $serviceId;
+        }
     }
 }
